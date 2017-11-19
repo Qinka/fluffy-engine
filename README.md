afadfb0fd367594ff21dd67dea8acd79f7db4edc
Johann Lee <qinka@live.com>
2017-11-19 16:51:08 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fluffy-engine
 A fluffy engine for XDU 2017 fall SS SPM final exam.
+
+[![Build Status](https://travis-ci.org/Qinka/fluffy-engine.svg?branch=master)](https://travis-ci.org/Qinka/fluffy-engine)
+[![license](https://img.shields.io/github/license/qinka/fluffy-engine.svg)](https://github.com/Qinka/fluffy-engine/blob/master/LICENSE)
+[![Docker image](https://img.shields.io/badge/docker-image-orange.svg)](https://hub.docker.com/r/qinka/fluffy/)
+[![ImageLayers Layers](https://img.shields.io/imagelayers/layers/qinka/fluffy/fluffy-latest.svg)](https://hub.docker.com/r/qinka/fluffy/)
+[![ImageLayers Size](https://img.shields.io/imagelayers/image-size/qinka/fluffy/fluffy-latest.svg)](https://hub.docker.com/r/qinka/fluffy/)
--- integration/ci.sh/succ.sh
+++ integration/ci.sh/succ.sh
@@ -4,37 +4,46 @@ set -e
 
 echo skip pull request
 if [ x"$TRAVIS_PULL_REQUEST" == "xfalse" ]; then
-  echo set up tag
-  if [ -n "$TRAVIS_TAG" ]; then
-    export GIT_TAG=$TRAVIS_TAG
-  else
-    export GIT_TAG=$TRAVIS_BRANCH-${TRAVIS_COMMIT:0:7}
-  fi
+    echo set up tag
+    if [ -n "$TRAVIS_TAG" ]; then
+        export GIT_TAG=$TRAVIS_TAG
+    else
+        export GIT_TAG=$TRAVIS_BRANCH-${TRAVIS_COMMIT:0:7}
+    fi
 
-  echo clean stack build
-  stack clean
+    echo clean stack build
+    stack clean
+    stack install cabal-install
 
-  if [ -n "$IS_DOCKER" ]; then
-    echo build docker image
-    echo
-    echo create folder
-    cd $TRAVIS_BUILD_DIR
-    mkdir -p docker.tmp/root
-    echo build fluffy  image
-    cd $TRAVIS_BUILD_DIR
-    stack install fluffy --ghc-options -O2 --ghc-options -threaded
-    export GIT_TAG=`echo $GIT_TAG | sed 's/\//-/g'`
-    export LATEST=latest
-    export FLUFFY=fluffy
-    echo copy files
-    sudo cp -r $HOME/.local/*  docker.tmp/root
-    sudo cp $TRAVIS_BUILD_DIR/integration/dockerfiles/fluffy.dockerfile   docker.tmp
-    cd docker.tmp
-    docker build -t qinka/fluffy:$FLUFFY-$GIT_TAG   -f fluffy.dockerfile              . || true
-    docker tag      qinka/fluffy:$FLUFFY-$GIT_TAG   qinka/fluffy:$FLUFFY-$LATEST    || true
-    echo push docker images
-    docker push qinka/fluffy || true
-  else
-    echo skip building docker image
-  fi
+    if [ -n "$IS_DOCKER" ]; then
+        echo build docker image
+        echo
+
+        echo create folder
+        cd $TRAVIS_BUILD_DIR
+        mkdir -p docker.tmp/root
+
+        echo build fluffy  image
+        cd $TRAVIS_BUILD_DIR
+        echo build fluffy
+        cd fluffy
+        cabal configure --prefix='/usr' --datasubdir='fluffy' --enable-optimization=3 --ghc-options="-thread"
+        cabal build
+        cabal copy --destdir=$TRAVIS_BUILD_DIR/docker.tmp/root
+        cd $TRAVIS_BUILD_DIR
+
+        export GIT_TAG=`echo $GIT_TAG | sed 's/\//-/g'`
+        export LATEST=latest
+        export FLUFFY=fluffy
+        echo copy files
+        sudo cp $TRAVIS_BUILD_DIR/integration/dockerfiles/fluffy.dockerfile   docker.tmp
+        
+        cd docker.tmp
+        docker build -t qinka/fluffy:$FLUFFY-$GIT_TAG   -f fluffy.dockerfile          . || true
+        docker tag      qinka/fluffy:$FLUFFY-$GIT_TAG   qinka/fluffy:$FLUFFY-$LATEST    || true
+        echo push docker images
+        docker push qinka/fluffy || true
+    else
+        echo skip building docker image
+    fi
 fi
